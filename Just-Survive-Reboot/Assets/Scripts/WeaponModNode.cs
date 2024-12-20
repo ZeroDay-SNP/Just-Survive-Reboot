5e631d621a680470ebb860e0fd99b77fcaa86eb9
zach-sousa-dev <zacharysousa@outlook.com>
2024-12-19 22:09:01 -0500

    added ray from camera to world point

--- Just-Survive-Reboot/Assets/Scripts/WeaponModNode.cs
+++ Just-Survive-Reboot/Assets/Scripts/WeaponModNode.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 
 public class WeaponModNode : MonoBehaviour
 {
-    public Camera cam;
-    Vector3 pos = new Vector3(200, 200, 0);
-
-    [field: SerializeField] private List<WeaponModPart> modOptions { get; }
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +14,6 @@ public class WeaponModNode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Cursor.lockState = CursorLockMode.Confined;
-        pos = Input.mousePosition;
-        Ray ray = cam.ScreenPointToRay(pos);
-        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        
     }
 }
--- Just-Survive-Reboot/Assets/Scripts/WeaponModder.cs
+++ Just-Survive-Reboot/Assets/Scripts/WeaponModder.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class WeaponModder : MonoBehaviour
 {
+    public Camera cam;
+    Vector3 pos;
+
+    [field: SerializeField] private List<WeaponModPart> modOptions { get; }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,10 @@ public class WeaponModder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
+        pos = Input.mousePosition;
+        Ray ray = cam.ScreenPointToRay(pos);
+        Debug.DrawRay(ray.origin, ray.origin + ray.direction * 10, Color.red);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
     }
 }
